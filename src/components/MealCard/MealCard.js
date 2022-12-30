419c8466a744281c9afb852023201a25f8415bda
f-rahim <freelancerrahim26@gmail.com>
2022-12-30 12:00:20 +0600

    Addeded eye icon

--- src/components/MealCard/MealCard.js
+++ src/components/MealCard/MealCard.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./MealCard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 const MealCard = ({ meal }) => {
   return (
     <div className="meal-card">
       <img src={meal.strMealThumb} alt={meal.strMeal} />
       <div className="body">
-        <h4>{meal.strMeal}</h4>
+        <h4>
+          {meal.strMeal}
+          <span>
+            <FontAwesomeIcon icon={faEye} />
+          </span>
+        </h4>
       </div>
     </div>
   );
